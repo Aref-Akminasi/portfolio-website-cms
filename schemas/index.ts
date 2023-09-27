45f2db39174cb4a4fd8e9934d67ef80e03e14d7f
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-09-27 16:57:47 +0200

    deleted unused schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,9 +1,5 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
 import aboutMe from './aboutMe'
 import skillset from './skillset'
 import projects from './projects'
 
-export const schemaTypes = [post, author, category, blockContent, aboutMe, skillset, projects]
+export const schemaTypes = [aboutMe, skillset, projects]
